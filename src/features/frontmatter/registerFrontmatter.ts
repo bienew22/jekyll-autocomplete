adb6554c59500830186ca5e50e031a7dad9c281c
BieNew22 <newbie11004@gmail.com>
2026-05-01 14:51:00 +0900

    feat: 파일 수정 이벤트 추가

--- src/features/frontmatter/registerFrontmatter.ts
+++ src/features/frontmatter/registerFrontmatter.ts
@@ -1,14 +1,19 @@
 import * as vscode from 'vscode';
-import { frontmatterHandler } from './frontmatterHandler';
+import { onCreateFileHandler, onRenameeFileHandler } from './frontmatterHandler';
+
 
 
 export function registerFrontmatter(context: vscode.ExtensionContext) {
     // 파일 생성시 frontmatter 자동 주입.
     const onCreate = vscode.workspace.onDidCreateFiles(async (event) => {
-        frontmatterHandler(event);
+        onCreateFileHandler(event);
     });
 
-
+    // 파일 이름 수정시 frontmatter 자동 변경.
+    const onChange = vscode.workspace.onDidRenameFiles(async (event) => {
+        onRenameeFileHandler(event);
+    });
 
     context.subscriptions.push(onCreate);
+    context.subscriptions.push(onChange);
 }
