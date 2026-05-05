90a198719c6e84c791a6c36ec50d408339f6b211
BieNew22 <newbie11004@gmail.com>
2026-05-05 21:34:35 +0900

    feat: 마크 다운 문법 자동완성 완성

--- src/features/completion/completion.register.ts
+++ src/features/completion/completion.register.ts
@@ -14,23 +14,4 @@ export function completionRegister(context: vscode.ExtensionContext) {
     
 
     context.subscriptions.push(markdownCompletion);
-
-//     vscode.workspace.onDidChangeTextDocument(event => {
-//     const editor = vscode.window.activeTextEditor;
-    
-//     if (!editor) {
-//         return;
-//     }
-
-//     const change = event.contentChanges[0];
-    
-//     if (!change) {
-//         return;
-//     }
-    
-//     // "/" 입력 감지
-//     if (change.text === '/') {
-//         vscode.commands.executeCommand('editor.action.triggerSuggest');
-//     }
-// });
 }
--- src/features/completion/markdown.completion.provider.ts
+++ src/features/completion/markdown.completion.provider.ts
@@ -14,19 +14,31 @@ const COMMANDS: MarkdownCommand[] = [
   { sort: "002", label: "h3", desc: "제목3", insert: "### " },
   { sort: "003", label: "h4", desc: "제목4", insert: "#### " },
 
-  { sort: "010", label: "desc", desc: "설명 리스트", insert: "$1\n: $2" },
+  { sort: "010", label: "desc-list", desc: "설명 리스트", insert: "$1\n: $2" },
+  { sort: "011", label: "todo-list", desc: "할일 리스트", insert: "- [$1] $2" },
 
-  { sort: "020", label: "prompt-tip", desc: "안내 프롬프트", insert: "> $1\n{: prompt-tip }" },
-  { sort: "021", label: "prompt-info", desc: "설명 프롬프트", insert: "> $1\n{: prompt-info }" },
-  { sort: "022", label: "prompt-warning", desc: "주의 프롬프트", insert: "> $1\n{: prompt-warning }" },
-  { sort: "023", label: "prompt-danger", desc: "위험 프롬프트", insert: "> $1\n{: prompt-danger }" },
+  { sort: "020", label: "prompt-tip", desc: "안내 프롬프트", insert: "> $1\n{: .prompt-tip }" },
+  { sort: "021", label: "prompt-info", desc: "설명 프롬프트", insert: "> $1\n{: .prompt-info }" },
+  { sort: "022", label: "prompt-warning", desc: "주의 프롬프트", insert: "> $1\n{: .prompt-warning }" },
+  { sort: "023", label: "prompt-danger", desc: "위험 프롬프트", insert: "> $1\n{: .prompt-danger }" },
 
   { sort: "030", label: "code", desc: "코드 블럭", insert: "```$1\n$2\n```" },
   { sort: "031", label: "code-noline", desc: "줄 번호 없는 코드 블럭", insert: "```$1\n$2\n```\n{: .nolineno }" },
   { sort: "032", label: "code-filename", desc: "파일명 채운 코드 블럭", insert: "```$1\n$3\n```\n{: .file='$2' }" },
   { sort: "033", label: "code-noline-filename", desc: "줄 번호 없는 파일명 채운 코드 블럭", insert: "```$1\n$3\n```\n{: .file='$2' .nolineno }" },
   
-  { sort: "040", label: "img", desc: "이미지", insert: "```$1\n$2\n```" },
+  { sort: "040", label: "img", desc: "이미지", insert: "![${1:alt}](${2:url})" },
+  { sort: "041", label: "img-with-caption", desc: "이미지에 캡션 존재", insert: "![${1:alt}](${2:url})\n_$3_" },
+  { sort: "042", label: "img-with-option", desc: "이미지에 옵션 추가", insert: "![${1:alt}](${2:url}){: w=\"$3\" }" },
+
+  { sort: "050", label: "footnote", desc: "각주", insert: "[^${1:각주 이름}]\n[^${2:각 이름}]: ${3:각주 설명}" },
+
+  { sort: "060", label: "link", desc: "하이퍼 링크", insert: "<${1:링크 주소}>" },
+  { sort: "061", label: "link-with-desc", desc: "하이퍼 링크 표시할 이름 추가", insert: "[${1:링크 이름}](${2:링크 주소})" },
+
+  { sort: "070", label: "table-order-center", desc: "가운데 정렬 표", insert: "| ${1:제목} |\n| :---: |\n | ${2:내용} |" },
+  { sort: "071", label: "table-order-left", desc: "왼쪽 정렬 표", insert: "| ${1:제목} |\n| :--- |\n | ${2:내용} |" },
+  { sort: "072", label: "table-order-right", desc: "오른쪽 정렬 표", insert: "| ${1:제목} |\n| ---: |\n | ${2:내용} |" },
 
   { sort: "100", label: "hello", desc: "test", insert: "![${1:alt}](${2:url})" },
 ];
